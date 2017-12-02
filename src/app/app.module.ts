872c7d53661b8aec74108f58b3532e9947396b58
Jose Evanan <jose.evanan@gmail.com>
2017-12-02 08:42:52 -0500

    update pestor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,27 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
-import { TabsPage } from '../pages/tabs/tabs';
-import { Camera } from '@ionic-native/camera';
 
 import { File } from '@ionic-native/file';
 import { Transfer } from '@ionic-native/transfer';
 import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
 
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+
 
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   imports: [
     BrowserModule,
@@ -32,10 +27,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
-    ContactPage,
-    HomePage,
-    TabsPage
+    HomePage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavController, ActionSheetController, ToastController, Platform, LoadingController, Loading } from 'ionic-angular';
- 
+
 import { File } from '@ionic-native/file';
 import { Transfer, TransferObject } from '@ionic-native/transfer';
 import { FilePath } from '@ionic-native/file-path';
 import { Camera } from '@ionic-native/camera';
- 
-declare var cordova: any;}
+import { Cloudinary } from 'cloudinary';
+declare var cordova: any;
+const cloudinary = Cloudinary;
 //https://devdactic.com/ionic-2-images/
 @Component({
   selector: 'page-home',
@@ -15,9 +16,11 @@ declare var cordova: any;}
 export class HomePage {
   lastImage: string = null;
   loading: Loading;
- 
-  constructor(public navCtrl: NavController, private camera: Camera, private transfer: Transfer, private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController, public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) { }
- 
+
+  constructor(public navCtrl: NavController, private camera: Camera, private transfer: Transfer,
+              private file: File, private filePath: FilePath, public actionSheetCtrl: ActionSheetController,
+              public toastCtrl: ToastController, public platform: Platform, public loadingCtrl: LoadingController) { }
+
   public presentActionSheet() {
     let actionSheet = this.actionSheetCtrl.create({
       title: 'Select Image Source',
@@ -42,4 +45,120 @@ export class HomePage {
     });
     actionSheet.present();
   }
+  public takePicture(sourceType) {
+  // Create options for the Camera Dialog
+  var options = {
+    quality: 100,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false,
+    correctOrientation: true
+  };
+
+  // Get the data of an image
+  this.camera.getPicture(options).then((imagePath) => {
+    // Special handling for Android library
+    if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+      this.filePath.resolveNativePath(imagePath)
+        .then(filePath => {
+          let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+          let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        });
+    } else {
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+    }
+  }, (err) => {
+    this.presentToast('Error while selecting image.');
+  });
+}
+
+// Create a new name for the image
+private createFileName() {
+  var d = new Date(),
+  n = d.getTime(),
+  newFileName =  n + ".jpg";
+  return newFileName;
+}
+
+// Copy the image to a local folder
+private copyFileToLocalDir(namePath, currentName, newFileName) {
+  this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.lastImage = newFileName;
+  }, error => {
+    this.presentToast('Error while storing file.');
+  });
+}
+
+private presentToast(text) {
+  let toast = this.toastCtrl.create({
+    message: text,
+    duration: 3000,
+    position: 'top'
+  });
+  toast.present();
+}
+
+// Always get the accurate path to your apps folder
+public pathForImage(img) {
+  if (img === null) {
+    return '';
+  } else {
+    return cordova.file.dataDirectory + img;
+  }
+}
+
+public uploadImage() {
+  // Destination URL
+  //var url = "http://yoururl/upload.php";
+
+
+  /*cloudinary.config({
+  cloud_name: 'gento',
+  api_key: '237728889441848',
+  api_secret: 'I8flkK2HkYES3E-c-RRa28nowg0'
+})*/
+  var url = "http://51.141.167.97:8000/save_image/"
+  console.log(this.lastImage)
+  //cloudinary.uploader.upload(this.lastImage, (result) => {
+  //  console.log(result)
+  //})
+
+  // File for Upload
+  var targetPath = this.pathForImage(this.lastImage);
+
+  // File name only
+  var filename = this.lastImage;
+
+  var options = {
+    fileKey: "file",
+    fileName: filename,
+    chunkedMode: false,
+    mimeType: "multipart/form-data",
+    params : {'fileName': filename}
+  };
+
+  const fileTransfer: TransferObject = this.transfer.create();
+
+  this.loading = this.loadingCtrl.create({
+    content: 'Uploading...',
+  });
+  this.loading.present();
+  console.log("-------");
+  console.log(targetPath);
+  console.log(url);
+  console.log(options);
+  console.log("-------");
+  // Use the FileTransfer to upload the image
+  fileTransfer.upload(targetPath, url, options).then(data => {
+    this.loading.dismissAll()
+    this.presentToast('Image succesful uploaded.');
+  }, err => {
+    this.loading.dismissAll()
+    this.presentToast('Error while uploading file.');
+  });
+}
+
+
 }
